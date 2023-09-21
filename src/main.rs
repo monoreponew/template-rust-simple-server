70807fbbac85bf78cef4897d5b438b3c5f73590e
Siggi Simonarson <siggisim@gmail.com>
2023-09-21 15:06:32 -0700

    Update src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,5 +15,12 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let response_contents = String::from("Hello, world!");
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        response_contents.len(),
+        response_contents
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
